86ed6277821ce86ea90b0a579eceb0bbb2aa01b2
Neil Hammerle <neilhammerle@Neils-MacBook-Pro.local>
2020-01-16 03:21:35 -0800

    server listening on 8080

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log("Server listening on http://localhost: " + PORT);
 })
