d04acfb68c24dfd25842a8b7f27eb351ac3bf795
Neil Hammerle <neilhammerle@Neils-MacBook-Pro.local>
2020-01-16 02:30:01 -0800

    Silicon Valley case added

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+    {
+        name: "Richard Hendricks",
+        photo: "https://cnet1.cbsistatic.com/img/HS3XPUTZux4P4cArqVywFhwRvpw=/1200x675/2019/08/17/9e246866-e3c9-4070-a8fa-466f0c685d49/siliconvalleyteaser.jpg",
+        scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    },
+    {
+        name: "Jared Dunn",
+        photo: "https://medias.spotern.com/spots/w640/7/7261-1532336916.jpg",
+        scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+    },
+    {
+        name: "Dinesh",
+        photo: "https://media.gq.com/photos/5df7dce2d0a40200081ee07e/16:9/w_2560%2Cc_limit/story-.jpg",
+        scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+    },
+    {
+        name: "Gilfoyle",
+        photo: "https://images-na.ssl-images-amazon.com/images/I/81ebQMRQR8L._SX679_.jpg",
+        scores: [3, 1, 3, 5, 2, 3, 2, 2, 4, 3]
+    },
+    {
+        name: "Erlich Bachman",
+        photo: "https://static.techspot.com/images2/news/bigimage/2017/05/2017-05-26-image-3.jpg",
+        scores: [1, 1, 2, 3, 2, 4, 3, 1, 1, 2]
+    }
+];
+
+module.exports = friends;
